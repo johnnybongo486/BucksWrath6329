4873fa6dfaee25522905e21564eddda2880cf288
johnnybongo486 <john.boynton@rsu25.org>
2023-01-19 14:40:39 -0500

    Falcons!

---
+++ Beta/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,99 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.Auto.*;
+import frc.robot.Commands.*;
+import frc.robot.Subsystems.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+    /* Controllers */
+    private final Joystick driver = new Joystick(0);
+    private final Joystick operator = new Joystick(1);
+
+    /* Drive Controls */
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Driver Buttons */
+    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
+    private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+
+    /* Subsystems */
+    private final Swerve s_Swerve = new Swerve();
+    public static Elevator elevator = new Elevator();
+    public static Wrist wrist = new Wrist();
+
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        s_Swerve.setDefaultCommand(
+            new TeleopSwerve(
+                s_Swerve, 
+                () -> -driver.getRawAxis(translationAxis), 
+                () -> -driver.getRawAxis(strafeAxis), 
+                () -> -driver.getRawAxis(rotationAxis), 
+                () -> robotCentric.getAsBoolean()
+            )
+        );
+
+        elevator.setDefaultCommand(new JoystickElevator());
+        wrist.setDefaultCommand(new JoystickWrist());
+
+        // Configure the button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        /* Driver Buttons */
+        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
+    }
+
+    public Joystick getDriver() {
+        return driver;
+    }
+
+    public Joystick getOperator() {
+        return operator;
+    }
+
+    public static double stickDeadband(double value, double deadband, double center) {
+        return (value < (center + deadband) && value > (center - deadband)) ? center : value;
+    }
+
+    public double getOperatorLeftStickY() {
+        return stickDeadband(this.operator.getRawAxis(1), 0.05, 0.0);
+    }
+ 
+    public double getOperatorRightStickY() {
+        return stickDeadband(this.operator.getRawAxis(5), 0.05, 0.0);
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return new ExampleAuto(s_Swerve);
+    }
+}
---
+++ Beta/src/main/java/frc/robot/Subsystems/Elevator.java
@@ -0,0 +1,232 @@
+package frc.robot.Subsystems;
+
+import frc.lib.models.*;
+import frc.robot.Robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase implements IPositionControlledSubsystem {
+
+	private boolean isHoldingPosition = false;
+
+    // Set Different Heights
+	private int homePosition = 0;
+	private int climbPosition = 28000;
+	private int raiseRobotPosition = 0;
+	private int maxUpTravelPosition = 30000;
+	private int nextBarPosition = 28000;
+
+	public final static int Elevator_UP = 0;
+	public final static int Elevator_DOWN = 1;
+
+	public int upPositionLimit = maxUpTravelPosition;
+	public int downPositionLimit = -1;
+	private int targetPosition = 0;
+	private double arbitraryFeedForward = 0.0;
+
+	private final static int onTargetThreshold = 100;
+	
+	private final SRXGains upGains = new SRXGains(Elevator_UP, 0.3, 0, 0, 0.011, 100);//0.2, 0, 0, 0.05, 100
+	private final SRXGains downGains = new SRXGains(Elevator_DOWN, 1.0, 0, 0, 0.011, 100);//0.1, 0, 0, 0.05, 100
+
+	//Uses PID values to go to a position
+	private MotionParameters highGearUpMotionParameters = new MotionParameters(180000, 90000, upGains);
+	private MotionParameters highGearDownMotionParameters = new MotionParameters(180000, 90000, downGains);
+	
+	private double peakOutputReverse = -1.0;
+	
+	private CustomTalonFX elevatorESC = new CustomTalonFX(16);
+
+	public Elevator() {
+		elevatorESC.configFactoryDefault();
+		elevatorESC.clearStickyFaults();
+
+		this.elevatorESC.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+	
+		this.elevatorESC.configForwardSoftLimitEnable(true);
+		this.elevatorESC.configForwardSoftLimitThreshold(upPositionLimit);
+
+		this.elevatorESC.configReverseSoftLimitEnable(true);
+		this.elevatorESC.configReverseSoftLimitThreshold(downPositionLimit);
+
+		this.elevatorESC.setInverted(false);
+		this.elevatorESC.setSensorPhase(false);
+
+		this.elevatorESC.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10);
+		this.elevatorESC.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10);
+
+		this.elevatorESC.configMotionParameters(highGearUpMotionParameters);
+        this.elevatorESC.configMotionParameters(highGearDownMotionParameters);
+
+		this.elevatorESC.setNeutralMode(NeutralMode.Brake);
+		this.elevatorESC.configClosedloopRamp(0.25);
+
+		this.elevatorESC.configVoltageCompSaturation(11.5);
+		this.elevatorESC.enableVoltageCompensation(true);
+
+		this.elevatorESC.configPeakOutputReverse(peakOutputReverse);
+
+		this.resetElevatorEncoder();
+	}
+
+	//sets control mode to motion magic
+	public void setElevator(ControlMode controlMode, double signal) {
+		if (controlMode == ControlMode.MotionMagic) {
+			this.manageMotion(signal);
+		}
+		elevatorESC.set(controlMode, signal);
+	}
+
+	public void setElevator(ControlMode controlMode, double signal, DemandType demandType, double demand) {
+		if (controlMode == ControlMode.MotionMagic) {
+			this.manageMotion(signal);
+		}
+		elevatorESC.set(controlMode, signal, demandType, demand);
+	}
+
+	public void motionMagicControl() {
+		this.manageMotion(targetPosition);
+		this.elevatorESC.set(ControlMode.MotionMagic, targetPosition, DemandType.ArbitraryFeedForward, arbitraryFeedForward);
+	}
+
+	public double getCurrentPosition() {
+		return this.elevatorESC.getSelectedSensorPosition();
+	}
+
+	public double getCurrentDraw() {
+		return this.elevatorESC.getSupplyCurrent();
+	}
+
+	public boolean isHoldingPosition() {
+		return this.isHoldingPosition;
+	}
+
+	public void setIsHoldingPosition(boolean isHoldingPosition) {
+		this.isHoldingPosition = isHoldingPosition;
+	}
+
+	public double getTargetPosition() {
+		return this.targetPosition;
+	}
+
+	public boolean setTargetPosition(int position) {
+		if (!isValidPosition(position)) {
+			return false;
+		} else {
+			this.targetPosition = position;
+			return true;
+		}
+	}
+
+	public void forceSetTargetPosition(int position) {
+		this.targetPosition = position;
+	}
+
+	public void incrementTargetPosition(int increment) {
+		int currentTargetPosition = this.targetPosition;
+		int newTargetPosition = currentTargetPosition + increment;
+		if (isValidPosition(newTargetPosition)) {		// && isWristSafe(newTargetPosition) check for other subsystems
+			this.targetPosition = newTargetPosition;
+		}
+	}
+
+	public boolean isValidPosition(int position) {
+		boolean withinBounds = position <= upPositionLimit && position >= downPositionLimit;
+		return withinBounds;
+	}
+
+    // communicate with commands
+	public int getHomePosition() {
+		return this.homePosition;
+	}
+
+	public int getMaxUpTravelPosition() {
+		return this.maxUpTravelPosition;
+	}
+
+	public int getClimbPosition() {
+		return this.climbPosition;
+	}
+
+	public int getRaiseRobotPosition() {
+		return this.raiseRobotPosition;
+	}
+
+	public int getNextBarPosition() {
+		return this.nextBarPosition;
+	}
+
+	public double getArbitraryFeedForward() {
+		return this.arbitraryFeedForward;
+	}
+
+	public void resetElevatorEncoder() {
+        try {
+			elevatorESC.setSelectedSensorPosition(0, 0, 30);
+        }
+        catch (Exception e) {
+            DriverStation.reportError("Elevator.resetElevatorEncoders exception.  You're Screwed! : " + e.toString(), false);
+        }
+	}
+
+	public double JoyStickElevator(){
+		double value = 0;
+		value = Robot.m_robotContainer.getOperatorLeftStickY();
+		return value;
+	}
+
+	public double getPositionError() {
+		double currentPosition = this.getCurrentPosition();
+		double targetPosition = this.getTargetPosition();
+		double positionError = Math.abs(currentPosition - targetPosition);
+		return positionError;
+	}
+
+	public void manageMotion(double targetPosition) {
+		double currentPosition = getCurrentPosition();
+		if (currentPosition < targetPosition) {
+				elevatorESC.selectMotionParameters(highGearUpMotionParameters);
+		}
+		else {
+				elevatorESC.selectMotionParameters(highGearDownMotionParameters);
+		}
+	}
+
+	public void zeroTarget() {
+		targetPosition = 0;
+	}
+
+	public void updateDashboard() {
+		SmartDashboard.putNumber("Elevator Position", this.getCurrentPosition());
+		SmartDashboard.putNumber("Elevator Target Position", this.getTargetPosition());
+		SmartDashboard.putNumber("Elevator Position Error", this.getPositionError());
+		SmartDashboard.putNumber("Elevator Velocity", this.getCurrentVelocity());
+		SmartDashboard.putNumber("Elevator Current", this.getCurrentDraw());
+		SmartDashboard.putNumber("Elevator Voltage", this.elevatorESC.getMotorOutputVoltage());
+	}
+
+	@Override
+	public double getCurrentVelocity() {
+		double currentVelocity = this.elevatorESC.getSelectedSensorVelocity();
+		return currentVelocity;
+	}
+
+	@Override
+	public boolean isInPosition(int targetPosition) {
+		double currentPosition = this.getCurrentPosition();
+		double positionError = Math.abs(currentPosition - targetPosition);
+		if (positionError < onTargetThreshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
